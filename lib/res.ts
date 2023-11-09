257b94ee709ee2be65a213708f708a64e164eeb4
maddsua <maddsuadev@proton.me>
2023-11-09 23:43:13 +0200

    Update res.ts

--- lib/res.ts
+++ lib/res.ts
@@ -1,10 +1,18 @@
-import { resolve } from "https://deno.land/std@0.205.0/path/mod.ts";
-import { normalize } from "https://deno.land/std@0.205.0/path/normalize.ts";
+import { normalize } from "https://deno.land/std@0.205.0/path/mod.ts";
 
 export const collapseWhitespaces = (html: string) => html.replace(/[\t ]+/g, ' ').replace(/[\r\n]/g, '');
 
 export const loadResource = (file: string, root?: string): string => {
+
 	const resourceLoadPath = normalize((root || Deno.cwd() + '/') + file);	
-	const content = Deno.readTextFileSync(resourceLoadPath);
+
+	let content: string;
+
+	try {
+		content = Deno.readTextFileSync(resourceLoadPath);
+	} catch (_error) {
+		throw new Error(`JSX failed to bundle resource: could not read file "${file}"; full path: "${resourceLoadPath}"`);
+	}
+
 	return collapseWhitespaces(content);
 };
