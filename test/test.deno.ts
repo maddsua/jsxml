5969681ed46624c0e7c732ab5416b43d40f3263a
maddsua <maddsuadev@proton.me>
2023-12-21 23:33:46 +0200

    Create test.deno.ts

---
+++ test/test.deno.ts
@@ -0,0 +1,26 @@
+import Component from "./templates/Component.tsx";
+
+const outputHTMLFile = './test/dist/index.html';
+
+try {
+	
+
+	const appRoot = Component();
+	let html = appRoot.render({
+		externalResourcesRoot: './test/resources/'
+	});
+
+	
+	if (!Deno.env.get('TEST_OUT_DRY')) {
+		Deno.writeTextFileSync(outputHTMLFile, html);
+		console.log('Written to:', outputHTMLFile)
+	}
+	else {
+		console.log(html);
+		console.log('✅ Rendered html ok, length:', html.length);
+	}
+
+} catch (error) {
+	console.error(error.message);
+	Deno.exit(1);
+}
