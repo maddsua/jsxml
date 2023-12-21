33ba6f3547712f06350463b9070e45043ff5e62e
maddsua <maddsuadev@proton.me>
2023-12-21 23:38:27 +0200

    Create test.node.ts

---
+++ test/test.node.ts
@@ -0,0 +1,28 @@
+import fs from 'fs';
+
+import Component from "./templates/Component.js";
+
+const outputHTMLFile = './test/dist/index.html';
+
+try {
+	
+
+	const appRoot = Component();
+	let html = appRoot.render({
+		externalResourcesRoot: './test/resources/'
+	});
+
+	
+	if (process.env['TEST_OUT_DRY'] !== 'true') {
+		fs.writeFileSync(outputHTMLFile, html);
+		console.log('Written to:', outputHTMLFile)
+	}
+	else {
+		console.log(html);
+		console.log('✅ Rendered html ok, length:', html.length);
+	}
+
+} catch (error) {
+	console.error(error.message);
+	process.exit(1);
+}
